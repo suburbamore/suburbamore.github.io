d55ef632452fe2e7db3658fa9d89f877f2c3e20a
Alice B <aliceb@Alices-MacBook-Pro.local>
2022-03-11 23:36:29 +0000

    I'm finally done the bare bones bitsgit add -A

--- scrollscript.js
+++ scrollscript.js
@@ -1,13 +1,12 @@
-var lastScrollTop = 0;
-var theWrap = $('.wrap');
-$(window).scroll(function(event){
-   var st = $(this).scrollTop();
-   if (st > lastScrollTop){
-       // downscroll code
-       theWrap.removeClass('sticky');
-   } else {
-      // upscroll code
-       theWrap.addClass('sticky');
-   }
-   lastScrollTop = st;
+const checkpoint = 300;
+ 
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= checkpoint) {
+    opacity = 1 - currentScroll / checkpoint;
+  } else {
+    opacity = 0;
+  }
+  document.querySelector(".front").style.opacity = opacity;
 });
+
