7f12439565f2a2583e37e0c233bc176e98bdae77
Alice Bibaud <alicebibaud@Alices-MacBook-Pro-2.local>
2022-07-26 15:37:30 -0700

    finally fixed the weirdness regarding opening bandmate descriptions

---
+++ script.js
@@ -0,0 +1,28 @@
+function columnFunction1(imgs) {
+  // Get the image text
+  let img = document.getElementById("img1");
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  img.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  img.parentElement.style.display = "block";
+} 
+
+
+function columnFunction2(imgs) {
+  // Get the image text
+  let img = document.getElementById("img2");
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  img.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  img.parentElement.style.display = "block";
+} 
+
+
+function columnFunction3(imgs) {
+  // Get the image text
+  let img = document.getElementById("img3");
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  img.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  img.parentElement.style.display = "block";
+} 
